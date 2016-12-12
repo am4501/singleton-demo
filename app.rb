5891a5253e759885a7e5a54d1415cbfd5f7f1368
Aleksey Voronkov <voronkov@gmail.com>
2016-12-12 09:39:07 +0200

    Final tune

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require './logger'
 
-Logger.say_something
+
 Logger.instance.log_something 'bla bla'
 Logger.instance.log_something 'bla bla 2'
 
--- logger.rb
+++ logger.rb
@@ -1,21 +1,16 @@
+require 'singleton'
+
 class Logger
+	include Singleton
+
 	def initialize
 		@f = File.open 'log.txt', 'a'
 	end
 
-	@@x = Logger.new
-
-	def self.instance
-		return @@x
-	end
-
-	def self.say_something
-		puts "haha"
-	end
-
+	
 	def log_something wat
 		@f.puts wat
+		@f.flush
 	end
 
-	private_class_method :new
 end
